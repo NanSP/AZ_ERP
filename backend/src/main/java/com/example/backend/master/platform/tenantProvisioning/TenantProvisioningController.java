9d4355ac6d4d3ab34a1d00f35edb457f7916626b
Hernandes S. Pereira <hpereira23@outlook.com>
2026-04-01 15:35:34 -0300

    add Provisioning base on master

---
+++ backend/src/main/java/com/example/backend/master/platform/tenantProvisioning/TenantProvisioningController.java
@@ -0,0 +1,4 @@
+package com.example.backend.master.platform.tenantProvisioning;
+
+public class TenantProvisioningController {
+}
---
+++ backend/src/main/java/com/example/backend/master/platform/tenantProvisioning/TenantProvisioningRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.backend.master.platform.tenantProvisioning;
+
+public record TenantProvisioningRequestDTO() {
+}
