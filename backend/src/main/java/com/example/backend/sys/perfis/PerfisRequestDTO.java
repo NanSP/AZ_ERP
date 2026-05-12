ff38821f7c73b738172dee5095c3f56da3511893
Hernandes S. Pereira <hpereira23@outlook.com>
2026-05-12 10:48:54 -0300

    minor fix in PerfisRequestDTO

--- backend/src/main/java/com/example/backend/sys/perfis/PerfisRequestDTO.java
+++ backend/src/main/java/com/example/backend/sys/perfis/PerfisRequestDTO.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 public record PerfisRequestDTO
         (
-                Integer id,
                 String nome,
                 String descricao,
                 Integer nivelAcesso,
