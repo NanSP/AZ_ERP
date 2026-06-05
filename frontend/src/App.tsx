ea4c6528a6f8dac0f4cb1f8f202a122986caf6e3
Hernandes S. Pereira <hpereira23@outlook.com>
2026-06-05 13:55:19 -0300

    update: Logins Pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import PasswordChangeGuard from "./auth/PasswordChangeGuard";
+import SiteLayout from "./layouts/SiteLayout";
 import Home from "./pages/Home/Home";
 import ModulePage from "./pages/ModulePage";
 import EntryPage from "./pages/EntryPage/EntryPage";
@@ -14,18 +15,20 @@ export default function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<EntryPage />} />
-          <Route path="/login" element={<MasterLoginPage />} />
-          <Route path="/tenant-login" element={<TenantLoginPage />} />
-          <Route path="/change-password" element={<ChangePasswordPage />} />
+          <Route element={<SiteLayout />}>
+            <Route path="/" element={<EntryPage />} />
+            <Route path="/login" element={<MasterLoginPage />} />
+            <Route path="/tenant-login" element={<TenantLoginPage />} />
+            <Route path="/change-password" element={<ChangePasswordPage />} />
 
-          <Route element={<ProtectedRoute />}>
-            <Route element={<PasswordChangeGuard />}>
-              <Route path="/app" element={<Home />} />
-              <Route
-                path="/app/module/:schema/:entity"
-                element={<ModulePage />}
-              />
+            <Route element={<ProtectedRoute />}>
+              <Route element={<PasswordChangeGuard />}>
+                <Route path="/app" element={<Home />} />
+                <Route
+                  path="/app/module/:schema/:entity"
+                  element={<ModulePage />}
+                />
+              </Route>
             </Route>
           </Route>
         </Routes>
